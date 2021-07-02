80a7989eb44945571c17e632c3363fed05dd159d
Tobias von Klipstein <tobias@klpstn.com>
2021-07-02 13:01:15 +0200

    Improve builtin drag-handle during dragging

--- glomex-dialog.js
+++ glomex-dialog.js
@@ -220,17 +220,13 @@ class GlomexDialogElement extends window.HTMLElement {
       display: none;
     }
 
-    .drag-handle:hover {
+    .drag-handle:hover, .drag-handle:active {
       background-color: rgba(0, 0, 0, 0.4);
     }
 
-    .drag-handle:active .drag-handle-overlay {
-      position: fixed;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      background: rgba(255, 255, 255, 0.4);
+    .drag-handle:active {
+      opacity: 1;
+      cursor: move;
     }
 
     .drag-handle svg {
@@ -305,7 +301,6 @@ class GlomexDialogElement extends window.HTMLElement {
         <slot name="dialog-element"></slot>
         <slot name="dock-overlay">
           <div class="drag-handle">
-            <div class="drag-handle-overlay"></div>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrows-move" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M7.646.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 1.707V5.5a.5.5 0 0 1-1 0V1.707L6.354 2.854a.5.5 0 1 1-.708-.708l2-2zM8 10a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 14.293V10.5A.5.5 0 0 1 8 10zM.146 8.354a.5.5 0 0 1 0-.708l2-2a.5.5 0 1 1 .708.708L1.707 7.5H5.5a.5.5 0 0 1 0 1H1.707l1.147 1.146a.5.5 0 0 1-.708.708l-2-2zM10 8a.5.5 0 0 1 .5-.5h3.793l-1.147-1.146a.5.5 0 0 1 .708-.708l2 2a.5.5 0 0 1 0 .708l-2 2a.5.5 0 0 1-.708-.708L14.293 8.5H10.5A.5.5 0 0 1 10 8z"/></svg>
           </div>
         </slot>

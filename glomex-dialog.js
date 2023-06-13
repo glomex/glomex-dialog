c37b1d8581e927bdb0714cf059dc6f844f52f8ee
Tobias von Klipstein <tobias@klpstn.com>
2023-06-13 21:09:03 +0200

    fix: try to set a height for the inner-wrapper

--- glomex-dialog.js
+++ glomex-dialog.js
@@ -317,6 +317,7 @@ class GlomexDialogElement extends window.HTMLElement {
       top: 0;
       left: 0;
       width: 100%;
+      height: 100%;
       max-width: 100%;
       will-change: transition, transform, width, height;
     }
